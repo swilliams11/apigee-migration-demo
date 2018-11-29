c400e0373906a880f1eebad97908b8a906f539d4
Sean Williams <williamssean@williamssean-macbookpro.roam.corp.google.com>
2018-11-29 15:47:07 -0600

    updated startup script and jinja files to use the startup script property.

--- gcp-deployment-manager/startup-scripts/express-gateway.sh
+++ gcp-deployment-manager/startup-scripts/express-gateway.sh
@@ -9,10 +9,20 @@ sudo npm install -g express-gateway
 #create the symlink
 sudo ln -s /opt/nodejs/lib/node_modules/express-gateway/bin/index.js /usr/bin/eg
 
+#install monitoring agent and stackdriver agent
+sudo curl -sSO https://dl.google.com/cloudagents/install-monitoring-agent.sh
+sudo bash install-monitoring-agent.sh
+
+sudo curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+sudo bash install-logging-agent.sh
+
 #init express-gateway
 cd ~
 eg gateway create -n my-gateway -d my-gateway -t basic
 
 #fetch the config file
-sudo curl https://github.com
+sudo curl https://raw.githubusercontent.com/swilliams11/apigee-migration-demo/master/gcp-deployment-manager/config/gateway-config.yaml -o my-gateway/gateway-config.yaml
+
 #start express gateway in the background
+cd my-gateway
+nohup npm start &
