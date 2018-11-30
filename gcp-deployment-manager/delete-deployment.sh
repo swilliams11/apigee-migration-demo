291a6a3ff35a304cb7cf66d88096ddcc6817a490
Sean Williams <williamssean@williamssean-macbookpro.roam.corp.google.com>
2018-11-30 15:07:46 -0600

    this code will create all the required resources and allow you to send a request to the HTTP LB and get a helloworld response from the express gateway/nodejs backend.

--- gcp-deployment-manager/delete-deployment.sh
+++ gcp-deployment-manager/delete-deployment.sh
@@ -1,5 +1,5 @@
-BUCKET_NAME="apigee-migration-demo"
-gsutil rm gs://$BUCKET_NAME/express-gateway.sh
+BUCKET_NAME="apigee-migration-demo5"
+#gsutil rm gs://$BUCKET_NAME/express-gateway.sh
 gsutil rm -r gs://$BUCKET_NAME
 
 gcloud deployment-manager deployments delete migration-deployment
--- gcp-deployment-manager/deploy-all.sh
+++ gcp-deployment-manager/deploy-all.sh
@@ -13,11 +13,11 @@
 #      edge-grouptemp --named-ports "http:9001"
 
 #gcloud compute health-checks create tcp NAME [--check-interval=CHECK_INTERVAL; default="5s"] [--description=DESCRIPTION] [--healthy-threshold=HEALTHY_THRESHOLD; default=2] [--proxy-header=PROXY_HEADER; default="NONE"] [--request=REQUEST] [--response=RESPONSE] [--timeout=TIMEOUT; default="5s"] [--unhealthy-threshold=UNHEALTHY_THRESHOLD; default=2] [--port=PORT; default=80 --port-name=PORT_NAME] [GCLOUD_WIDE_FLAG …]
-BUCKET_NAME="apigee-migration-demo"
-gsutil mb gs://$BUCKET_NAME/
+BUCKET_NAME="apigee-migration-demo5"
+gsutil mb -c regional -l us-central1 gs://$BUCKET_NAME/
 #gsutil rm gs://apigee-migration-demo/express-gateway.sh
-gsutil cp startup-scripts/express-gateway.sh gs://$BUCKET_NAME
-gsutil acl ch -u AllUsers:R gs://$BUCKET_NAME/express-gateway.sh
+gsutil cp startup-scripts/\*.sh gs://$BUCKET_NAME
+gsutil acl ch -u AllUsers:R gs://$BUCKET_NAME/\*.sh
 
 gcloud deployment-manager deployments create migration-deployment --config migration-demo.yaml
 
